8f7d3033ffafc68b82089ebc19d8c405b3200385
AbdoulLatif153 <kouadioabdoullatif@gmail.com>
2025-01-27 19:54:18 +0000

    Create serializers.py

---
+++ backend/cv/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import CV, Experience, Education, Skill
+
+class ExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Experience
+        fields = '__all__'
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = '__all__'
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+class CVSerializer(serializers.ModelSerializer):
+    experiences = ExperienceSerializer(many=True, read_only=True)
+    education = EducationSerializer(many=True, read_only=True)
+    skills = SkillSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = CV
+        fields = '__all__'
